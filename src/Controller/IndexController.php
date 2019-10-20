495c9dfddf363a9c75b822e6ff2a2565b3ce0600
henrique221 <hborges9294@gmail.com>
2019-10-20 04:57:28 -0300

    adding google maps

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     * @IsGranted("ROLE_USER")
+     */
+    public function index()
+    {
+        return $this->render('index/index.html.twig', [
+            'controller_name' => 'IndexController',
+        ]);
+    }
+}
