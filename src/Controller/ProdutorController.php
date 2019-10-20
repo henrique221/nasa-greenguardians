fa1e5079924d1970ef8e3987d24fedb47f18ad08
henrique221 <hborges9294@gmail.com>
2019-10-20 12:36:54 -0300

    User

--- src/Controller/ProdutorController.php
+++ src/Controller/ProdutorController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Produtor;
 use App\Entity\Propriedade;
 use App\Form\ProdutorType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
@@ -12,6 +13,21 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ProdutorController extends AbstractController
 {
+
+    /**
+     * @Route("/produtor/{id}", name="produtor_index")
+     * @param Produtor $produtor
+     * @IsGranted("ROLE_PRODUTOR", message="Acesso não permitido")
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    public function indexProdutor(Produtor $produtor)
+    {
+        if($this->getUser() == $produtor->getUser()){
+            return $this->render("produtor/show.html.twig", array("produtor" => $produtor));
+        }
+        return $this->redirectToRoute("produtor_index", ["id" => $this->getUser()->getProdutor()]);
+    }
+
     /**
      * @Route("/produtor/new", name="produtor_new")
      */
@@ -36,7 +52,8 @@ class ProdutorController extends AbstractController
             $propriedade = new Propriedade();
             $propriedade->setLocalizacao($request->request->get('local'));
             $propriedade->setProdutorId($produtor);
-
+            $propriedade->setTipoProducao($request->get('tipo'));
+            $produtor->addPropriedade($propriedade);
             $this->getDoctrine()->getRepository(Produtor::class)->save($produtor);
             return $this->redirectToRoute('index');
         }
